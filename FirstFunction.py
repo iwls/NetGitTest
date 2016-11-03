c3ec4ac4ef1821460f01375fa19d953091ce7b19
iwls <ian.willems@gmail.com>
2016-11-03 01:25:34 +0100

    upload of first functions

---
+++ FirstFunction.py
@@ -0,0 +1,12 @@
+#this my first function
+def triangle_are(base, height):
+    area=(1.0/2)*base *height
+    return area
+
+def share_price(unitprice, number_shares):
+    shareprice= ((unitprice*number_shares) + 7.25 + 2.54)/number_shares
+    return shareprice
+
+
+
+
